1f13c356ada19a3ab9711c7c5eeac3db39f78237
henriquebueno45 <80426500+henriquebueno45@users.noreply.github.com>
2022-10-04 22:39:11 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import pic from "./linhaBase.png";
-//import linhaBase from 'C:\Users\henri\my-app\public';
+import linhaBase from './linhaBase.png';
 import Draggable from 'react-draggable';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="App-header">
-           <img src={pic} class='App-logo' alt="car"/>
+            <img src={ linhaBase } class="linhaHorizontal" alt="car"/ >
+            <img src={ linhaBase } class="linhaVertical" alt="car"/ >
           <Draggable>
-              <div className="chaveamento">
-                  <div>--| |--</div>
+              <div className="items">
+                  <div>-| |-</div>
               </div>
           </Draggable>
+
           <Draggable>
-              <div className="bobina">
-                  <div>--( )--</div>
+              <div className="items">
+                  <div>-( )-</div>
               </div>
           </Draggable>
+
+          <Draggable>
+              <div className="items">
+                  <div>-|/|-</div>
+              </div>
+          </Draggable>
+
       </div>
     </div>
 
